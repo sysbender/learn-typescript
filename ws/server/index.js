2bc7bc99c9ae980b814eaae67b97d43eb8bd51db
Jason Meng <sysbender@gmail.com>
2024-09-29 12:32:51 -0400

    websocket chat

---
+++ ws/server/index.js
@@ -0,0 +1,11 @@
+// const ws = require("ws");
+import { WebSocketServer } from "ws";
+
+const server = new WebSocketServer({ port: 3000 });
+server.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    const b = Buffer.from(message);
+    console.log(b.toString());
+    socket.send(`${message}`);
+  });
+});

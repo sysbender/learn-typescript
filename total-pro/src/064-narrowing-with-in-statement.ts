5ca0a95ff53e2142791e7444e247073dac962820
Jason Meng <sysbender@gmail.com>
2025-01-25 18:15:43 -0500

    node crash

--- total-pro/src/064-narrowing-with-in-statement.ts
+++ total-pro/src/064-narrowing-with-in-statement.ts
@@ -1,6 +1,9 @@
+import { it } from "vitest";
+
 type APIResponse = { data: { id: string } } | { error: string };
 
 const handleResponse = (response: APIResponse) => {
+  // property in object
   if ("data" in response) {
     return response.data.id;
   } else {
@@ -8,3 +11,10 @@ const handleResponse = (response: APIResponse) => {
   }
 };
 
+it("should handle a response with data", () => {
+  const response = {
+    data: {
+      id: "123",
+    },
+  };
+});
